f24f176304499b7fede08f0dd84831d9f23afb93
Nguyen <nguyentrungnguyen43@gmail.com>
2025-08-26 04:17:21 +0700

    nguyen

--- src/main/java/AI_PRJ/WEBAPP/repository/ShipRepo.java
+++
@@ -1,9 +0,0 @@
-package AI_PRJ.WEBAPP.repository;
-
-import AI_PRJ.WEBAPP.model.Ship;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShipRepo extends JpaRepository<Ship, Integer> {
-}
